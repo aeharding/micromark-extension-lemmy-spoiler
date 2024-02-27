8c19e1d7ddc1e49557ffd86f4b188550f24e7482
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-02-26 20:12:18 -0600

    Progress

--- dev/index.ts
+++ dev/index.ts
@@ -1,6 +1,9 @@
-/// <reference path="./lib/types.d.ts" />
-/// <reference path="./lib/mdast.d.ts" />
+/// <reference path="./lib/micromark/types.d.ts" />
+/// <reference path="./lib/mdast/mdast.d.ts" />
 
-export { directive } from "./lib/syntax.js";
-export { directiveHtml } from "./lib/html.js";
-export { directiveFromMarkdown, directiveToMarkdown } from "./lib/mdast.js";
+export { directive } from "./lib/micromark/syntax.js";
+export { directiveHtml } from "./lib/micromark/html.js";
+export {
+  directiveFromMarkdown,
+  directiveToMarkdown,
+} from "./lib/mdast/mdast.js";
